d3fe64b90a8c364c545dbeb4a3497ab58e0b5436
Navdeep Singh <navdeep.singh@ing.com>
2025-03-11 09:49:34 +0100

    added text manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import LineChartComponent from "@/components/chart";
 import { useEffect, useState } from "react";
 import Savings from "@/utils/provider";
+import { DataTableDemo } from "@/components/table"
+
 
 export default function Home() {
   const [savings, setSaving] = useState();
@@ -16,9 +18,12 @@ export default function Home() {
 
   return (
     <main>
-      <div className="w-96">
-        <LineChartComponent></LineChartComponent>
+      <div className="flex max-sm:col-span-2">
+      <div className="w-96 m-2"><LineChartComponent></LineChartComponent></div>
+      <div className="w-96 m-2"><LineChartComponent></LineChartComponent></div>
+      <div className="w-96 m-2"><LineChartComponent></LineChartComponent></div>
       </div>
+      <DataTableDemo></DataTableDemo>
     </main>
   );
 }
--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -17,20 +17,25 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import {useTranslations} from 'next-intl';
+import { useState } from "react";
+
+
 
 const chartData = [
-  { stipendi: "January", desktop: 0, mobile: 80 },
-  { stipendi: "February", desktop: 1299, mobile: 200 },
-  { stipendi: "March", desktop: 1299, mobile: 120 },
-  { stipendi: "April", desktop: 1299, mobile: 190 },
+  { stipendi: "January", desktop: 2141, mobile: 80 },
+  { stipendi: "February", desktop: 2141, mobile: 200 },
+  { stipendi: "March", desktop: 2141, mobile: 120 },
+  { stipendi: "April", desktop: 2299, mobile: 190 },
   { stipendi: "May", desktop: 2141, mobile: 130 },
   { stipendi: "June", desktop: 2100, mobile: 140 },
 ];
 
+
 const chartConfig = {
   desktop: {
-    label: "Stipendio",
-    color: "black",
+    label: "Entra",
+    color:  chartData[chartData.length-1].desktop >= chartData[chartData.length-2].desktop ? "green" : "red",
   },
   mobile: {
     label: "Mobile",
@@ -39,14 +44,21 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 export default function LineChartComponent() {
+  const [saldo, setSaldo] = useState(1.99); 
+  const t = useTranslations('Chart');
   return (
+   
     <Card style={{ background: "#f6f5f4" }}>
       <CardHeader>
-        <CardTitle>Net worth</CardTitle>
+        <CardTitle>
+          <section>
+            <h1>{t('networth')}</h1>
+          </section>
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <div>
-          <p className="font-semibold text-5xl">€83.00</p>
+          <p className="font-semibold text-5xl">€{saldo}</p>
         </div>
         <ChartContainer config={chartConfig}>
           <LineChart
@@ -56,7 +68,9 @@ export default function LineChartComponent() {
               top: 10,
               left: 10,
               right: 10,
+              bottom: 10,
             }}
+            height={5}
           >
             <ChartTooltip
               cursor={true}
@@ -67,7 +81,7 @@ export default function LineChartComponent() {
               type="natural"
               stroke="var(--color-desktop)"
               strokeWidth={2}
-              dot={false}
+              dot={true}
             />
           </LineChart>
         </ChartContainer>
@@ -77,10 +91,7 @@ export default function LineChartComponent() {
           style={{ color: "#009E60" }}
           className="flex gap-2 font-normal leading-none"
         >
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          {t('chart_desc_up',{perc:"10"})} <TrendingUp className="h-4 w-4" />
         </div>
       </CardFooter>
     </Card>
